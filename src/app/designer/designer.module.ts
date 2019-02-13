d070def8d652825c074dfd830e6c90e03a5fb5a9
Chaim Cheng <chengch@inspur.com>
2019-02-13 17:17:02 +0800

    commit

--- src/app/designer/designer.module.ts
+++ src/app/designer/designer.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DesignerComponent } from './designer.component';
 import { DesignerRoutingModule } from './designer-routing.module';
+import { HeaderComponent } from './shell/header/header.component';
+import { SidebarComponent } from './shell/sidebar/sidebar.component';
+import { ContentComponent } from './shell/content/content.component';
+import { FooterComponent } from './shell/footer/footer.component';
+import { AsideComponent } from './shell/aside/aside.component';
 
 @NgModule({
   imports: [
     CommonModule,
     DesignerRoutingModule
   ],
-  declarations: [DesignerComponent]
+  declarations: [DesignerComponent, HeaderComponent, SidebarComponent, ContentComponent, FooterComponent, AsideComponent]
 })
 export class DesignerModule { }
