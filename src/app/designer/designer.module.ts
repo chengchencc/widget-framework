139dc35c62e692535a6ab30e963db5951b5d9e84
Chaim Cheng <chengch@inspur.com>
2019-02-14 16:33:22 +0800

    调整设计器样式

--- src/app/designer/designer.module.ts
+++ src/app/designer/designer.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DesignerComponent } from './designer.component';
 import { DesignerRoutingModule } from './designer-routing.module';
+//common vendors
+import { TabsModule } from 'ngx-bootstrap/tabs';
+//child component
 import { HeaderComponent } from './shell/header/header.component';
 import { SidebarComponent } from './shell/sidebar/sidebar.component';
 import { ContentComponent } from './shell/content/content.component';
@@ -11,7 +14,8 @@ import { AsideComponent } from './shell/aside/aside.component';
 @NgModule({
   imports: [
     CommonModule,
-    DesignerRoutingModule
+    DesignerRoutingModule,
+    TabsModule.forRoot()
   ],
   declarations: [DesignerComponent, HeaderComponent, SidebarComponent, ContentComponent, FooterComponent, AsideComponent]
 })
--- src/app/designer/shell/header/header.component.ts
+++ src/app/designer/shell/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'design-header',
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  // @Input() pageCofnig:PageConfig;
+
+  // @Output() onSave = new EventEmitter();
+  // @Output() onPreview = new EventEmitter<boolean>();
+
+  isPc = true;
+
+  registedThemes:{key:string,href:string}[]=[
+    {key:"default",href:"default.css"},
+    {key:"red",href:"/assets/themes/styles.css"}
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  save(){
+    // this.onSave.emit();
+  }
+
+  preview(){
+    // this.onPreview.emit();
+  }
+
+  themeChanged(event){
+    // console.log(event);
+    // document.getElementById("themecss").setAttribute("href",event.target.value);
+    // alert("theme changed!!!")
+  }
+
+  changeDevice(){
+    this.isPc = !this.isPc;
   }
 
 }
