b313fb583e936ec55e4514eef4b64d57f0b6d232
Toxiiic <872435708@qq.com>
2019-04-25 11:06:08 +0800

    refactor: remove old widget dir

--- projects/widget/ide/src/lib/directives/drag-input.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DragInputDirective } from './drag-input.directive';
-
-describe('DragInputDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DragInputDirective();
-    expect(directive).toBeTruthy();
-  });
-});
