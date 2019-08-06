601a1cf27a04f7a6b30eeca5bdfb62f65615d111
Toxiiic <872435708@qq.com>
2019-08-06 15:28:59 +0800

    update: 部件编辑器, 模板管理等页面更新为新 header

--- projects/widget-config/src/app/comps/public-header/public-header.component.ts
+++ projects/widget-config/src/app/comps/public-header/public-header.component.ts
@@ -9,13 +9,15 @@ interface HeaderBtn {
 @Component({
   selector: 'app-public-header',
   templateUrl: './public-header.component.html',
-  styleUrls: ['./public-header.component.scss']
+  styleUrls: ['./public-header.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PublicHeaderComponent implements OnInit {
 
   @Input() title: string
   @Input() btns: HeaderBtn[]
   @ContentChild('rightComp') rightComp: TemplateRef<ElementRef>
+  @ContentChild('leftComp') leftComp: TemplateRef<ElementRef>
 
   constructor() { }
 
